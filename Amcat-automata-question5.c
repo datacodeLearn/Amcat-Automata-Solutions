57ba4822fdc1b304aabe44ac1c3365a649b688ee
Datacode.in <69146457+datacodeLearn@users.noreply.github.com>
2020-08-07 20:48:14 +0530

    Amcat Automata Question-5 Solution

--- Amcat-automata-question5.c
+++ Amcat-automata-question5.c
@@ -1,5 +1,4 @@
-//A n-by-n matrix of 0’s and 1’s is given, where all 1’s in each row come before all 0’s, 
-//find the most efficient way to return the row with the maximum number of 0’s.
+//A n-by-n matrix of 0’s and 1’s is given, where all 1’s in each row come before all 0’s, find the most efficient way to return the row with the maximum number of 0’s.
 
 
 #include <stdio.h>
